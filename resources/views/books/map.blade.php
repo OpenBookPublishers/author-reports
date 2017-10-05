bd7ab6772d33dc25146b18e2e367c8ee3ba4ab12
Javier Arias <javier@arias.re>
2017-10-05 15:41:24 +0100

    add map

---
+++ resources/views/books/map.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+@section('title', 'Readership Map')
+@section('content')
+
+@include('layouts.dashboard-btn')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Readership map - {{ $book->title }}
+                    </div>
+
+                    <div class="panel-body">
+                        <div class="embed-responsive embed-responsive-4by3">
+                            <iframe class="embed-responsive-item"
+                                    src="{{ $map_url }}">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
