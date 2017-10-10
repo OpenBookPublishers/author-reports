00d0b790b7184711998be871f76e2466eabb7c09
Javier Arias <javier@arias.re>
2017-10-10 17:18:16 +0100

    fix typo

--- resources/views/layouts/status.blade.php
+++ resources/views/layouts/status.blade.php
@@ -3,7 +3,7 @@
         <div class="col-md-12">
                     @if (session('success'))
                         <div class="alert alert-success">
-                            {{ session('status') }}
+                            {{ session('success') }}
                         </div>
                     @endif
 
