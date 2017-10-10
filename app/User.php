ed805dc645f09b96d35fad16d1d0195b9a3087d2
Javier Arias <javier@arias.re>
2017-10-10 11:54:40 +0100

    validate authorship propertly

--- app/User.php
+++ app/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
      */
     public function isAuthor()
     {
-        return $this->author->count() > 0;
+        return $this->author->count() !== null;
     }
 
     /**
