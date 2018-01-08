6c6fdbb8da4f9d97315914f0c8d907e8ccc8c94c
Javier Arias <javier@arias.re>
2018-01-08 16:44:02 +0000

    use secondary btn yield for downloads

--- resources/views/books/report-headers.blade.php
+++ resources/views/books/report-headers.blade.php
@@ -1,8 +1,20 @@
-@extends('layouts.master')
+@extends('layouts.dashboard-btn')
 @section('title', 'Readership Graphs')
+@section('head')
+  <link rel="stylesheet"
+   href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+@endsection
 @section('content')
 
-@include('layouts.dashboard-btn')
+@section('secondary-btn')
+    <a data-toggle="modal"
+       data-target="#pdf-{{ $book->book_id }}"
+       class="btn btn-default pointer pull-right">
+            <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+            Download PDF
+    </a>
+    @include('pdf-selector')
+@endsection
 
     <div class="container">
         <div class="row">
