4cc60fa3eabc0090f736f7c02655033d7d7cde37
Javier Arias <javier@arias.re>
2018-01-08 16:49:03 +0000

    make stats tables responsive

--- resources/views/books/table.blade.php
+++ resources/views/books/table.blade.php
@@ -1,79 +1,80 @@
 <p class="section-header line-break">{{ $table['title'] }}</p>
-<table class="report-table table table-responsive">
-    <tr>
-      <td class="platform border">{{ $table['column'] }}</td>
-      @if ($year === null)
-        @foreach ($book->years_active as $year => $months)
-      <td class="border right">
-          @if ($is_pdf)
-          {{ $year }}
+<div class="table-responsive">
+    <table class="report-table table table-responsive">
+        <tr>
+          <td class="platform border">{{ $table['column'] }}</td>
+          @if ($year === null)
+            @foreach ($book->years_active as $year => $months)
+          <td class="border right">
+              @if ($is_pdf)
+              {{ $year }}
+              @else
+              <a href="{{ URL::route('report', ['book_id' => $book->book_id,
+                                                'year' => $year]) }}">
+                {{ $year }}
+              </a>
+              @endif
+          </td>
+            @endforeach
           @else
-          <a href="{{ URL::route('report', ['book_id' => $book->book_id,
-                                            'year' => $year]) }}">
-            {{ $year }}
-          </a>
+            @foreach ($book->years_active[$year] as $month => $blank)
+          <td class="border right">
+              {{ Carbon\Carbon::createFromFormat("m", $month)->format('M') }}
+          </td>
+            @endforeach        
           @endif
-      </td>
-        @endforeach
-      @else
-        @foreach ($book->years_active[$year] as $month => $blank)
-      <td class="border right">
-          {{ Carbon\Carbon::createFromFormat("m", $month)->format('M') }}
-      </td>
-        @endforeach        
-      @endif
-      <td class="border right">Totals</td>
-    </tr>
-    @foreach ($table['data'] as $platform => $stats)
-    <?php
-    $platform_total = 0;
-    $class = $platform === "Net Rev Total" || $platform === "Amount due"
-             ? "result right" : "right";
-    $platform_class = $platform === "Net Rev Total"
-        || $platform === "Amount due" ? "result" : "";
-    ?>
-    <tr>
-        <td class="{{ $platform_class }}">{{ $platform }}</td>
-        @foreach ($stats as $year => $stat)
+          <td class="border right">Totals</td>
+        </tr>
+        @foreach ($table['data'] as $platform => $stats)
         <?php
-        $platform_total += $stat;
-        if (!isset($table['years_total'][$year])) {
-            $table['years_total'][$year] = 0;
-        }
-        $table['years_total'][$year] += $stat;
+        $platform_total = 0;
+        $class = $platform === "Net Rev Total" || $platform === "Amount due"
+                 ? "result right" : "right";
+        $platform_class = $platform === "Net Rev Total"
+            || $platform === "Amount due" ? "result" : "";
         ?>
-        <td class="{{ $class }}">
-            <!-- FIXME hack for royalties paid -->
-            @if ($platform !== "Amount due")
-                @if (is_float($stat))
-                {{ number_format($stat, 2, '.', '') }}
-                @else
-                {{ $stat ? : "" }}
+        <tr>
+            <td class="{{ $platform_class }}">{{ $platform }}</td>
+            @foreach ($stats as $year => $stat)
+            <?php
+            $platform_total += $stat;
+            if (!isset($table['years_total'][$year])) {
+                $table['years_total'][$year] = 0;
+            }
+            $table['years_total'][$year] += $stat;
+            ?>
+            <td class="{{ $class }}">
+                <!-- FIXME hack for royalties paid -->
+                @if ($platform !== "Amount due")
+                    @if (is_float($stat))
+                    {{ number_format($stat, 2, '.', '') }}
+                    @else
+                    {{ $stat ? : "" }}
+                    @endif
                 @endif
-            @endif
-        </td>
+            </td>
+            @endforeach
+
+            <td class="{{ $class }}">
+                @if (is_float($stat))
+                {{ number_format($platform_total, 2, '.', '') }}
+                @else
+                {{ $platform_total ? : "" }}
+                @endif
+            </td>
+        </tr>
         @endforeach
-        
-        <td class="{{ $class }}">
-            @if (is_float($stat))
-            {{ number_format($platform_total, 2, '.', '') }}
-            @else
-            {{ $platform_total ? : "" }}
-            @endif
-        </td>
-    </tr>
-    @endforeach
-    @if ($table['totals_col'] !== "")
-    <tr>
-        <td class="result">{{ $table['totals_col'] }}</td>
-        @foreach ($table['years_total'] as $year => $total)
-        <?php
-        $table['global_total'] += $total;
-        ?>
-        <td class="result right">{{ $total ? : "" }}</td>
-        @endforeach
-        <td class="result right">{{ $table['global_total'] ? : "" }}</td>
-    </tr>
-    @endif
-</table>
-        
+        @if ($table['totals_col'] !== "")
+        <tr>
+            <td class="result">{{ $table['totals_col'] }}</td>
+            @foreach ($table['years_total'] as $year => $total)
+            <?php
+            $table['global_total'] += $total;
+            ?>
+            <td class="result right">{{ $total ? : "" }}</td>
+            @endforeach
+            <td class="result right">{{ $table['global_total'] ? : "" }}</td>
+        </tr>
+        @endif
+    </table>
+</div>
