307e8a3efd6e6cad24dae8e2978f17eb15189e32
Javier Arias <javier@arias.re>
2018-01-08 15:29:23 +0000

    move pdf select modal to a different view

---
+++ resources/views/pdf-selector.blade.php
@@ -0,0 +1,46 @@
+<div class="modal fade" id="pdf-select" role="dialog">
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close"
+                data-dismiss="modal">
+            &times;
+        </button>
+          <h4 class="modal-title">
+              Metrics report (PDF) - 
+              {{ $book->title }}
+          </h4>
+      </div>
+      <div class="modal-body text-center">
+        <a href="{{ URL::route('download-report',
+                               ['book_id' => $book->book_id]) }}"
+           class="btn btn-default half-width">
+            <i class="fa fa-file-pdf-o"
+               aria-hidden="true"></i>
+            Full report
+        </a>
+        <br>
+
+    @foreach ($book->getYearsActive() as $year => $months)
+
+        <a href="{{ URL::route('download-report',
+                            ['book_id' => $book->book_id, 'year' => $year]) }}"
+           class="btn btn-default line-break half half-width">
+            <i class="fa fa-file-pdf-o"
+               aria-hidden="true"></i>
+            {{ $year }}
+        </a>
+        <br>
+    @endforeach
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default"
+                data-dismiss="modal">
+            Close
+        </button>
+      </div>
+    </div>
+
+  </div>
+</div>
