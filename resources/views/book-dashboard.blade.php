eb26e769e1a8b7cc58117fbffeeeb6a33a083ab7
Javier Arias <javier@arias.re>
2018-01-08 15:25:56 +0000

    allow select year in pdf report

--- resources/views/book-dashboard.blade.php
+++ resources/views/book-dashboard.blade.php
@@ -21,9 +21,8 @@
                             </div>
                         </a>
 
-                        <a href="{{ route('download-report',
-                                    ['book_id' => $book->book_id]) }}"
-                           class="btn-large square relative">
+                        <a data-toggle="modal" data-target="#pdf-select"
+                           class="btn-large square relative pointer">
                             <div class="centered full-width">
                                 <i class="fa fa-file-pdf-o fa-large"
                                    aria-hidden="true"></i>
@@ -60,6 +59,51 @@
                                 </span>
                             </div>
                         </a>
+
+                        <div class="modal fade" id="pdf-select" role="dialog">
+                          <div class="modal-dialog">
+
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close"
+                                        data-dismiss="modal">
+                                    &times;
+                                </button>
+                                  <h4 class="modal-title">
+                                      Metrics report (PDF) - 
+                                      {{ $book->title }}
+                                  </h4>
+                              </div>
+                              <div class="modal-body text-center">
+                                <a href="{{ URL::route('download-report', ['book_id' => $book->book_id]) }}"
+                                   class="btn btn-default half-width">
+                                    <i class="fa fa-file-pdf-o"
+                                       aria-hidden="true"></i>
+                                    Full report
+                                </a>
+                                <br>
+
+                            @foreach ($book->getYearsActive() as $year => $months)
+
+                                <a href="{{ URL::route('download-report', ['book_id' => $book->book_id, 'year' => $year]) }}"
+                                   class="btn btn-default line-break half half-width">
+                                    <i class="fa fa-file-pdf-o"
+                                       aria-hidden="true"></i>
+                                    {{ $year }}
+                                </a>
+                                <br>
+                            @endforeach
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-default"
+                                        data-dismiss="modal">
+                                    Close
+                                </button>
+                              </div>
+                            </div>
+
+                          </div>
+                        </div>
 
                         @else
                         <p>
