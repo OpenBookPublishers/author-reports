89ce2283fed3f2f646c683f9587c7a4bbf225e27
Javier Arias <javier@arias.re>
2018-01-08 16:43:48 +0000

    make dashboard-btn a layout

--- resources/views/layouts/dashboard-btn.blade.php
+++ resources/views/layouts/dashboard-btn.blade.php
@@ -1,10 +1,18 @@
-    <div class="container" style="margin-bottom: 1em;">
-        <div class="row">
-            <div class="col-md-2">
-                <a class="btn btn-default" href="{{ route('dashboard') }}">
-                    <i class="glyphicon glyphicon-chevron-left"></i>
-                    Dashboard
-                </a>
-            </div>
+@extends('layouts.app')
+
+@include('layouts.nav')
+@include('layouts.status')
+<div class="container" style="margin-bottom: 1em;">
+    <div class="row">
+        <div class="col-md-2 pull-left">
+            <a class="btn btn-default" href="{{ route('dashboard') }}">
+                <i class="glyphicon glyphicon-chevron-left"></i>
+                Dashboard
+            </a>
         </div>
+        <div class="col-lg-2 pull-right">
+            @yield('secondary-btn')
+        </div>            
     </div>
+</div>
+@yield('content')
