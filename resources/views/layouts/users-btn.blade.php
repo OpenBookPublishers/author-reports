dccf1999f14b0cb956ed4fc037c1155615ad6555
Javier Arias <javier@arias.re>
2018-01-09 17:24:14 +0000

    turn users-btn into a template

--- resources/views/layouts/users-btn.blade.php
+++ resources/views/layouts/users-btn.blade.php
@@ -1,10 +1,20 @@
-    <div class="container" style="margin-bottom: 1em;">
-        <div class="row">
-            <div class="col-md-2">
-                <a class="btn btn-default" href="{{ route('admin-users') }}">
-                    <i class="glyphicon glyphicon-chevron-left"></i>
-                    Manage Users
-                </a>
-            </div>
+@extends('layouts.app')
+
+@section('app')
+@include('layouts.nav')
+@include('layouts.status')
+<div class="container" style="margin-bottom: 1em;">
+    <div class="row">
+        <div class="col-md-2 pull-left">
+            <a class="btn btn-default" href="{{ route('admin-users') }}">
+                <i class="glyphicon glyphicon-chevron-left"></i>
+                Manage Users
+            </a>
+        </div>
+        <div class="col-lg-2 pull-right">
+            @yield('secondary-btn')
         </div>
     </div>
+</div>
+@yield('content')
+@endsection
