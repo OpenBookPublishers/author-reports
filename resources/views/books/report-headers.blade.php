e218bbf6c005a50289d4f72eecfb1f3597d5a284
Javier Arias <javier@arias.re>
2018-01-11 11:22:58 +0000

    move report container to separate view

--- resources/views/books/report-headers.blade.php
+++ resources/views/books/report-headers.blade.php
@@ -11,24 +11,6 @@
 @endsection
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        {{ $year !== null ? $year . " " : "" }}
-                        Metrics report - {{ $book->title }}
-                    </div>
-
-                    <div class="panel-body">
-                        @include('books.report')
-                        <p class="text-muted">
-                            <i class="fa fa-info-circle" aria-hidden="true"></i> You may click on a year to view its monthly breakdown.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('books.report-container')
 
 @endsection
