8cec2a5b8e0572d02c6ebdc8345e2363912efe48
Javier Arias <javier@arias.re>
2018-01-11 13:48:54 +0000

    put graphs container in different view

--- resources/views/books/graphs-headers.blade.php
+++ resources/views/books/graphs-headers.blade.php
@@ -2,24 +2,6 @@
 @section('title', 'Readership Graphs')
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default" style="min-height: 30em;">
-                    <div class="panel-heading">
-                        Readership graphs - {{ $book->title }}
-                    </div>
-
-                    <div class="panel-body">
-                        <p class="text-muted line-break-double">
-                            N.B. Only readership for which we have geographical information is displayed. "Other" represents other countries.
-                        </p>
-                        @include('books.graphs')
-                        <div style="clear: both;"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('books.graphs-container')
 
 @endsection
