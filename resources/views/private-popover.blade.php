5bf7b5c3a09c5c6ea21f453a604ea8fa8113220e
Javier Arias <javier@arias.re>
2018-01-16 10:01:02 +0000

    add clarity to privacy message

--- resources/views/private-popover.blade.php
+++ resources/views/private-popover.blade.php
@@ -1,7 +1,7 @@
 <a name="private-popover" tabindex="0" role="button" data-toggle="popover"
    data-trigger="focus" title="Data privacy" class="pull-left"
    style="margin-right: -80px;" data-placement="right"
-   data-content="This data is not displayed publicly. You are logged-in and, therefore, have access to this data even in the public report.">
+   data-content="This data is not displayed publicly. You are currently logged-in and, therefore, have access to this data even in the public report.">
     <i class="fa fa-lg fa-lock" aria-hidden="true" style="color:"></i> Private
 </a>
 <script>
