5baca4e97b8435a85a6b6d3ebf2de88dc1d10ebd
Javier Arias <javier@arias.re>
2018-02-06 11:24:20 +0000

    add global cookie js functions

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,2 +1,35 @@
 require('./bootstrap');
 
+window.setCookie = function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+};
+
+window.getCookie = function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+
+window.dismissInfoForm = function dismissInfoForm() {
+    setCookie("dissmiss-info-form", true, 7);
+};
+
+window.hideDashboardFormIfCookie = function hideDashboardFormIfCookie() {
+    var dismiss = getCookie("dissmiss-info-form");
+    if (!dismiss) {
+        $('#info-form-container').removeClass('hidden');
+    }
+};
